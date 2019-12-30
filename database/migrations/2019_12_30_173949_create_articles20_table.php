8a2df389f80896675094d4ec58f1c105741b61ce
liujing@bogon <liukai5455@163.com>
2019-12-30 17:47:26 +0800

    new migrate

---
+++ database/migrations/2019_12_30_173949_create_articles20_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticles20Table extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        
+        $max = 30;
+        
+        for ($i=0; $i<$max; $i++) { 
+            Schema::create('articles_'.$i, function (Blueprint $table) {
+                $table->engine = 'InnoDB';
+                $table->charset = 'utf8mb4';
+                $table->collation = 'utf8mb4_general_ci';
+                $table->increments('id');
+                $table->integer('book_id')->unsigned()->default(0);
+                $table->string('parent_flag', 50);
+                $table->string('relation_flag', 50);
+                $table->string('origin_site', 80)->default('shuquge');
+                $table->string('title', 50);
+                $table->text('content');
+                $table->integer('sort_weight')->unsigned()->default(0);
+                $table->tinyInteger('is_del')->unsigned()->default(0);
+                $table->integer('create_time')->unsigned();
+                $table->index(['is_del', 'book_id']);
+                $table->index('book_id');
+                $table->index(['parent_flag', 'relation_flag', 'origin_site']);
+            });
+        }
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article');
+    }
+}
