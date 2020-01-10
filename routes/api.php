f112ba8a57ad1e87a95f0d7090c332338828a91c
liujing@bogon <liukai5455@163.com>
2020-01-10 11:57:07 +0800

    a

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-//use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,8 +12,7 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-/*
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-*/
