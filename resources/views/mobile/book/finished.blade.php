4997380cf00ba8c1baf93e4bf3270b8da9120d7f
liujing@bogon <liukai5455@163.com>
2020-01-16 18:36:17 +0800

    a

--- resources/views/mobile/book/finished.blade.php
+++ resources/views/mobile/book/finished.blade.php
@@ -70,8 +70,9 @@
 				loadList();
                 oScroll.on("pullingUp",function(){
                     loadList();
+                    oScroll.refresh();
                     oScroll.finishPullUp();//可以多次执行上拉刷新
                 });
-                oScroll.refresh();
+               
             </script>
 @endsection
